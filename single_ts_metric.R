83347c8e9587eae59a345740f3f0bc41e3153c5f
fzhang-WZ <67087493+fzhang-WZ@users.noreply.github.com>
2023-08-07 10:37:45 -0600

    Update single_ts_metric.R

--- single_ts_metric.R
+++ single_ts_metric.R
@@ -1,6 +1,6 @@
 #--------------------------------  NOTE  ----------------------------------------
 # 1 This code is to quantify single time series uncertainty based on different data sources;
-# 2 The format of the input data is [ERA WTK];
+# 2 The format of the input data is [ERA OBS];
 # 3 Coder: Flora Zhang         Date: 08/02/2023       @ NREL
 #--------------------------------------------------------------------------------
 
@@ -8,46 +8,48 @@ mv_single <- function(df) {
   library(ForeCA)
   library(fields)
   library(imputeTestbench)
-  # df <- data.frame(dataset[,c('ERA', 'WTK')])
+  library(zoo)
+  # df <- data.frame(dataset[,c('ERA', 'OBS')])
   # window size: 12
-   
+  
   # Moving-window Entropy
   en_era <- c()
-  en_wtk <- c()
+  en_obs <- c()
   for (i in 1:(dim(df)[1]-12)){
-     en_era <- ForeCA::spectral_entropy(df$ERA[i:(i+12)])
-     en_wtk <- ForeCA::spectral_entropy(df$WTK[i:(i+12)])
+    en_era[i] <- ForeCA::spectral_entropy(df$ERA[i:(i+12)])
+    en_obs[i] <- ForeCA::spectral_entropy(df$OBS[i:(i+12)])
   } 
-
+  
   # Moving-window Standard Deviation
   sd_era <- c()
-  sd_wtk <- c()
+  sd_obs <- c()
   for (i in 1:(dim(df)[1]-12)){
-     sd_era <- sd(df$ERA[i:(i+12)])
-     sd_wtk <- sd(df$WTK[i:(i+12)])
+    sd_era[i] <- sd(df$ERA[i:(i+12)])
+    sd_obs[i] <- sd(df$OBS[i:(i+12)])
   } 
   
   # Moving-window Turbulence Intensity
   ti_era <- c()
-  ti_wtk <- c()
+  ti_obs <- c()
   for (i in 1:(dim(df)[1]-12)){
-     ti_era <- (max(df$ERA[i:(i+12)])-min(df$ERA[i:(i+12)]))/mean(df$ERA[i:(i+12)])
-     ti_wtk <- (max(df$WTK[i:(i+12)])-min(df$WTK[i:(i+12)]))/mean(df$WTK[i:(i+12)])
+    ti_era[i] <- (max(df$ERA[i:(i+12)])-min(df$ERA[i:(i+12)]))/mean(df$ERA[i:(i+12)])
+    ti_obs[i] <- (max(df$OBS[i:(i+12)])-min(df$OBS[i:(i+12)]))/mean(df$OBS[i:(i+12)])
   } 
-    
+  
   # Moving-window Variability Index
   vi_era <- c()
-  vi_wtk <- c()
+  vi_obs <- c()
   for (i in 1:(dim(df)[1]-12)){
-     ti_era <- sd(df$ERA[i:(i+12)])/mean(df$ERA[i:(i+12)])
-     ti_wtk <- sd(df$WTK[i:(i+12)])/mean(df$WTK[i:(i+12)])
+    ti_era[i] <- sd(df$ERA[i:(i+12)])/mean(df$ERA[i:(i+12)])
+    ti_obs[i] <- sd(df$OBS[i:(i+12)])/mean(df$OBS[i:(i+12)])
   } 
   #result <- as.matrix(cbind(nCRPS, pb_ave, npb_ave, ACE, CE_98, reliability, sharpness))
-  result <- list('entropy' = cbind(en_era, en_wtk),  'sd' = cbind(sd_era, sd_wtk), 
-  'ti' = cbind(ti_era, ti_wtk), 'vi' = cbind(vi_era, vi_wtk))
+  result <- list('entropy' = cbind(en_era, en_obs),  'sd' = cbind(sd_era, sd_obs), 
+                 'ti' = cbind(ti_era, ti_obs), 'vi' = cbind(vi_era, vi_obs))
   
   return(result)
 }
+
 
 # visualize the metrics in heatmaps
 # given example: hourly data
@@ -56,21 +58,21 @@ sd_era.matrix <- matrix(sd_era, nrow = 365, ncol = 24)
 ti_era.matrix <- matrix(ti_era, nrow = 365, ncol = 24)
 vi_era.matrix <- matrix(vi_era, nrow = 365, ncol = 24)
 
-en_wtk.matrix <- matrix(en_wtk, nrow = 365, ncol = 24)
-sd_wtk.matrix <- matrix(sd_wtk, nrow = 365, ncol = 24)
-ti_wtk.matrix <- matrix(ti_wtk, nrow = 365, ncol = 24)
-vi_wtk.matrix <- matrix(vi_wtk, nrow = 365, ncol = 24)
+en_obs.matrix <- matrix(en_obs, nrow = 365, ncol = 24)
+sd_obs.matrix <- matrix(sd_obs, nrow = 365, ncol = 24)
+ti_obs.matrix <- matrix(ti_obs, nrow = 365, ncol = 24)
+vi_obs.matrix <- matrix(vi_obs, nrow = 365, ncol = 24)
 
 image.plot(1:365, 1:24, en_era.matrix, xlab = "day", ylab = "hour", main = "Entropy")
-image.plot(1:365, 1:24, en_wtk.matrix, xlab = "day", ylab = "hour", main = "Entropy")
+image.plot(1:365, 1:24, en_obs.matrix, xlab = "day", ylab = "hour", main = "Entropy")
 
 image.plot(1:365, 1:24, sd_era.matrix, xlab = "day", ylab = "hour", main = "Standard Deviation")
-image.plot(1:365, 1:24, sd_wtk.matrix, xlab = "day", ylab = "hour", main = "Standard Deviation")
+image.plot(1:365, 1:24, sd_obs.matrix, xlab = "day", ylab = "hour", main = "Standard Deviation")
 
 image.plot(1:365, 1:24, ti_era.matrix, xlab = "day", ylab = "hour", main = "Turbulence Intensity",zlim=c(0,1))
-image.plot(1:365, 1:24, ti_wtk, xlab = "day", ylab = "hour", main = "Turbulence Intensity",zlim=c(0,1))
+image.plot(1:365, 1:24, ti_obs, xlab = "day", ylab = "hour", main = "Turbulence Intensity",zlim=c(0,1))
 
 image.plot(1:365, 1:24, vi_era.matrix, xlab = "day", ylab = "hour", main = "Variability Index")
-image.plot(1:365, 1:24, vi_wtk.matrix, xlab = "day", ylab = "hour", main = "Variability Index")
+image.plot(1:365, 1:24, vi_obs.matrix, xlab = "day", ylab = "hour", main = "Variability Index")
 
 
